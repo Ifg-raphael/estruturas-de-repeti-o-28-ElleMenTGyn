46b639e4a8c18463c9f1775bd4f0eeb67939c67e
Francisco Neto <faguiar.eng@gmail.com>
2025-08-29 15:51:27 -0300

    Segue codigo

--- calcula_media.py
+++ calcula_media.py
@@ -1,1 +1,8 @@
-# Sua solução aqui
+soma = 0.0                 # variavel para acumular a soma dos números
+
+for _ in range(5):         # leitura de 5 valores
+    numero = float(input())  # lê um numero
+    soma += numero           # adiciona à soma
+
+media = soma / 5            # calcula a média(5)
+print(f"{media:.2f}")       # imprime a média com 2 casas decimais
